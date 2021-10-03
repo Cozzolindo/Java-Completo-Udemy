6be0a5e39c4c337787bfd18e2c9945dbcabfe03f
Cozzolindo <cozzolinocarlos@gmail.com>
2021-10-03 13:24:53 -0300

    Stack Trace

--- JavaException/Try-catch/Main.java
+++ JavaException/Try-catch/Main.java
@@ -3,6 +3,15 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
+        
+        method1();
+        System.out.println("The End of programmation");
+
+    }
+
+    public static void method2(){
+
+        System.out.println("***Method 2 STARTING***");
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Names:");
@@ -14,11 +23,18 @@ public class Main{
             System.out.println(vector[position]);
         } catch(ArrayIndexOutOfBoundsException e){
             System.out.println("Out of Bounds!");
+            e.printStackTrace();
         } catch(InputMismatchException e){
             System.out.println("Mismatched!");
+            e.printStackTrace();
         }
 
-        System.out.println("The End of programmation");
+        System.out.println("***Method 2 ENDING***");
+    }
 
+    public static void method1(){
+        System.out.println("***Method 1 STARTING***");
+        method2();
+        System.out.println("***Method 1 ENDING***");
     }
 }
